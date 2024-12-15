bec1818d24417142f5db2e5a232d60d8da2151d4
drngwj <weijiang-ng@hotmail.com>
2024-12-15 12:52:33 +0800

    app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello World"
+def os_info():
+    return f"CPU Count: {os.cpu_count()}, System: {os.name}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
